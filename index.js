47853aecefb41e7a29f3ddc5de5341d770af8e7d
hberkay <69199765+hberkay@users.noreply.github.com>
2024-02-01 14:26:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js"); 
 const client= new Discord.Client();    
-client.login("discord botunuzun tokenini giriniz"); 
+client.login("discord bot token :"); 
 const axios = require('axios'); 
 
 
